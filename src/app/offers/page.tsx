e0b85332083a35689fbbd546146c27f8570aa84d
Rithish Danda <rithishdanda@rithishs-Mac.local>
2024-06-02 19:07:07 +0530

    a

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -1,6 +1,4 @@
-// Use client-side rendering for this component
 "use client";
-
 import React, { useState } from 'react';
 import Link from 'next/link';
 
@@ -17,14 +15,14 @@ const tokens = [
   'LEMON-8790',
 ];
 
-const GridBox = ({ index }) => {
-  const [isClicked, setIsClicked] = useState(false);
-  const [token, setToken] = useState<string | null>(null);
+const GridBox = ({ index }: { index: number }) => {
+const [isClicked, setIsClicked] = useState(false);
+const [token, setToken] = useState<string | null>(null);
 
-  const handleClick = () => {
+const handleClick = () => {
     setIsClicked(true);
     setToken(tokens[Math.floor(Math.random() * tokens.length)]);
-  };
+};
 
   return (
     <div
@@ -42,32 +40,21 @@ const GridBox = ({ index }) => {
 };
 
 export default function Home() {
-  const [showGrid, setShowGrid] = useState(true); // State variable for grid visibility
-
-  const toggleGrid = () => {
-    setShowGrid(!showGrid); // Toggle grid visibility on button click
-  };
-
   return (
     <div className="container">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            <code className="font-mono font-bold">Home</code>
-          </p>
-        </Link>
-        {/* Button to toggle grid visibility */}
-        <button onClick={toggleGrid}>
-          {showGrid ? 'Hide Grid' : 'Show Grid'}
-        </button>
-      </div>
-      {showGrid && ( // Conditionally render the grid based on showGrid state
-        <div className="grid w-auto mb-32">
-          {[...Array(10)].map((_, index) => (
-            <GridBox key={index} index={index} />
-          ))}
+            <Link href={"/"}>
+              <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                <code className="font-mono font-bold">Home</code>
+              </p>
+            </Link>
         </div>
-      )}
+      <div className="grid w-auto mb-32">
+        {[...Array(10)].map((_, index) => (
+          <GridBox key={index} index={index} />
+        ))}
+      </div>
     </div>
   );
 }
+
